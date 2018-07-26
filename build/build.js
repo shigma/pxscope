6d89a1afff9480880862eadfabb021c6d797094e
Shigma <1700011071@pku.edu.cn>
2018-07-26 22:56:55 +0800

    add some scss

--- build/build.js
+++ build/build.js
@@ -2,12 +2,12 @@ const sass = require('sass')
 const path = require('path')
 const fs = require('fs')
 
-function transcode(filenames, directory = '', wrapper = code => code) {
+function transcode(filenames = [], wrapper = name => name, formatter = code => code) {
   filenames.forEach(filename => {
-    const filepath = path.join(__dirname, '..', directory, filename)
+    const filepath = path.join(__dirname, '..', wrapper(filename))
     const source = fs.readFileSync(filepath + '.scss', {encoding: 'utf8'})
     const css = sass.renderSync({
-      data: wrapper(source, filename),
+      data: formatter(source, filename),
       outputStyle: 'compressed'
     }).css
     fs.writeFileSync(filepath + '.css', css)
@@ -16,6 +16,12 @@ function transcode(filenames, directory = '', wrapper = code => code) {
 
 transcode(['app'])
 
-transcode(['night', 'simple'], 'themes', (code, theme) => {
-  return `.${theme}{${code}}`
-})
+transcode(['night', 'simple'],
+  (filename) => `themes/${filename}`,
+  (code, theme) => `.${theme}{${code}}`
+)
+
+transcode(['user', 'homepage', 'settings'],
+  (filename) => `comp/${filename}/index`,
+  (code, comp) => `.${comp}{${code}}`
+)
