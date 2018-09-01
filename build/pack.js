5821517e5cee62b64829d8aee6a6c694eece04d6
Shigma <1700011071@pku.edu.cn>
2018-09-01 23:57:20 +0800

    rcedit patch

--- build/pack.js
+++ build/pack.js
@@ -1,22 +1,15 @@
-const pj = require('../package.json')
+const meta = require('../package.json')
 const ep = require('electron-packager')
 const archiver = require('archiver')
 const cp = require('child_process')
+const semver = require('semver')
 const util = require('./util')
 const path = require('path')
 const fs = require('fs')
 
-const DIR_PATH = util.resolve(`pack/PxScope-v${pj.version}-win32-x64`)
+const INFO_PATH = util.resolve('build/info.json')
+const DIR_PATH = util.resolve(`pack/PxScope-v${meta.version}-win32-x64`)
 const ERROR_CODE = util.flag('slient') ? 0 : 1
-
-if (process.env.TRAVIS === 'true') {
-  try {
-    console.log('\nDownloading and installing wine ...\n')
-    cp.execFileSync(util.resolve('build/wine.sh'), { stdio: 'inherit' })
-  } catch (error) {
-    process.exit(ERROR_CODE)
-  }
-}
 
 function hook(callback) {
   return [(tempdir, version, platform, arch, next) => {
@@ -25,54 +18,77 @@ function hook(callback) {
   }]
 }
 
-util.start()
-util.clone('main.prod.js', 'main.js')
+!async function() {
+  if (process.platform !== 'win32') {
+    console.log('\n$ sh ./build/wine.sh')
+    const child = cp.execFile(util.resolve('build/wine.sh'))
+    child.stdout.on('data', console.log)
+    child.stderr.on('data', console.error)
+  }
+}().then(() => {
 
-if (fs.existsSync(DIR_PATH)) {
-  console.log('Directory already exists and will be removed first.\n')
-  util.remove(DIR_PATH)
-  util.remove(DIR_PATH + '.zip')
-}
+  const info = fs.existsSync(INFO_PATH) ? require(INFO_PATH) : {}
+  const rceditVersion = require('rcedit/package.json').version
+  if (!info.rcedit || semver.neq(rceditVersion, info.rcedit)) {
+    info.rcedit = rceditVersion
+    console.log('Downloading node-rcedit patch ...\n')
+    util.exec('curl -L -o node_modules/rcedit/bin/rcedit.exe https://github.com/electron/rcedit/releases/download/v1.1.0/rcedit-x64.exe')
+  }
+  fs.writeFileSync(INFO_PATH, JSON.stringify(info))
 
-ep({
-  appVersion: pj.version,
-  platform: 'win32',
-  arch: 'x64',
-  icon: util.resolve('assets/logo.icon'),
-  dir: util.resolve(),
-  executableName: 'PxScope',
-  ignore: [
-    /.+\.dev\..+/,
-    /.+\.js\.map/,
-    /.+\.d\.ts'/,
-    /test\..+/,
-    '.vscode',
-    '.eslintignore',
-    '.eslintrc.yml',
-    '.gitattributes',
-    '.gitignore',
-    '.gitmodules',
-    'tslint.yml',
-    'tsconfig.json',
-    '/package-lock.json',
-    '/assets/icons.svg',
-    '/assets/logo.ico',
-    '/assets/logo.svg',
-    '/pixiv/src',
-    '/build',
-    '/comp',
-    '/docs',
-  ],
-  name: `PxScope-v${pj.version}`,
-  out: path.join(__dirname, '../pack'),
-  prune: true,
-  afterCopy: hook(tempdir => console.log(`All files have been copied to ${tempdir}.`)),
-  afterExtract: hook(tempdir => console.log(`Electron has been extracted to ${tempdir}.`)),
-  afterPrune: hook(() => {
-    // FIXME: prune node modules.
-    console.log(`Prune Succeed. Waiting for files to copied into ${DIR_PATH} ...`)
-  }),
+}).then(() => {
+
+  util.start()
+  util.clone('main.prod.js', 'main.js')
+  
+  if (fs.existsSync(DIR_PATH)) {
+    console.log('Directory already exists and will be removed first.\n')
+    util.remove(DIR_PATH)
+    util.remove(DIR_PATH + '.zip')
+  }
+  
+  return ep({
+    appVersion: meta.version,
+    platform: 'win32',
+    arch: 'x64',
+    icon: util.resolve('assets/logo.icon'),
+    dir: util.resolve(),
+    executableName: 'PxScope',
+    ignore: [
+      /.+\.dev\..+/,
+      /.+\.js\.map/,
+      /.+\.d\.ts'/,
+      /test\..+/,
+      '.vscode',
+      '.eslintignore',
+      '.eslintrc.yml',
+      '.gitattributes',
+      '.gitignore',
+      '.gitmodules',
+      'tslint.yml',
+      'tsconfig.json',
+      '/package-lock.json',
+      '/assets/icons.svg',
+      '/assets/logo.ico',
+      '/assets/logo.svg',
+      '/pixiv/src',
+      '/build',
+      '/comp',
+      '/docs',
+    ],
+    name: `PxScope-v${meta.version}`,
+    out: path.join(__dirname, '../pack'),
+    prune: true,
+    afterCopy: hook(tempdir => console.log(`All files have been copied to ${tempdir}.`)),
+    afterExtract: hook(tempdir => console.log(`Electron has been extracted to ${tempdir}.`)),
+    afterPrune: hook(() => {
+      // FIXME: prune node modules.
+      console.log(`Prune Succeed. Waiting for files to copied into ${DIR_PATH} ...`)
+    }),
+  })
+
 }).then((_, error) => {
+
   util.clone('main.dev.js', 'main.js')
 
   if (error) throw error
@@ -95,6 +111,7 @@ ep({
     archive.directory(DIR_PATH)
     archive.finalize()
   }
+
 }).catch((error) => {
   console.error(error)
   process.exit(ERROR_CODE)
--- build/wine.sh
+++ build/wine.sh
@@ -1,7 +1,10 @@
-curl -o wine-3.0.2.tar.xz https://dl.winehq.org/wine/source/3.0/wine-3.0.2.tar.xz
-tar xf wine-3.0.2.tar.xz
-cd wine-3.0.2
-sudo apt-get update
-sudo apt-get install build-essential
-./configure
-make
+if [ wine64 -v sudo >/dev/null 2>&1 ]; then
+  echo Downloading and installing wine ...
+  curl -o wine-3.0.2.tar.xz https://dl.winehq.org/wine/source/3.0/wine-3.0.2.tar.xz
+  tar xf wine-3.0.2.tar.xz
+  cd wine-3.0.2
+  sudo apt-get update
+  sudo apt-get install build-essential
+  ./configure
+  make
+fi
